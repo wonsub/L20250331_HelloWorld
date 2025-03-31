d49fcc44d0ecdb8725e37a98a9651cc003d48c76
wonsub <secretfice@naver.com>
2025-03-31 16:39:11 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+//int
+//char
+//float
+//대입연산자
+//산술연산자
+//변수선언
+
+int main()
+{
+	int Number1 = 5;
+	int Number2 = 4;
+	float number;
+
+	float A = (float)5.5f;
+	int B = A;
+
+	
+	return 0;
+
+}
